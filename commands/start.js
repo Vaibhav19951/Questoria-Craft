fd0daa0ea767579a14ec0700fe3e0c296eb99adf
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-05-17 12:59:01 +0530

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -1,25 +1,22 @@
-const TelegramBot = require("node-telegram-bot-api");
-require("dotenv").config();
+// Remove the requires and bot initialization from here, 
+// because index.js already handles that!
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, {
-  polling: true,
-});
-
-bot.onText(/\/start/, async (msg) => {
-  const chatId = msg.chat.id;
-
-  try {
-    await bot.sendPhoto(
-      chatId,
-      "https://kommodo.ai/i/ip5xqtnqGqXGpuQxOpkZ",
-      {
-        caption: `⚔️ WELCOME TO DEMON SLAYER BOT ⚔️`,
-      }
-    );
-  } catch (err) {
-    console.log(err);
-    bot.sendMessage(chatId, "Error aa gaya start image me 😓");
-  }
-});
-
-console.log("Bot running...");
+module.exports = (bot) => { 
+  bot.onText(/\/start/, async (msg) => { 
+    const chatId = msg.chat.id; 
+   
+    try { 
+      await bot.sendPhoto( 
+        chatId, 
+        "https://kommodo.ai/i/ip5xqtnqGqXGpuQxOpkZ", 
+        { 
+          // Added quotation marks around the string!
+          caption: "⚔️ WELCOME TO DEMON SLAYER BOT ⚔️", 
+        } 
+      ); 
+    } catch (err) { 
+      console.log(err); 
+      bot.sendMessage(chatId, "Error aa gaya start image me 😓"); 
+    } 
+  }); 
+};
